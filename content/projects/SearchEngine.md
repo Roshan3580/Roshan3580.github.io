63216faf665b244d5a52969f2d342373482cf496
Roshan Raj <roshar1@uci.edu>
2025-07-06 15:34:19 +0530

    Deployed the first version of the website

---
+++ content/projects/SearchEngine.md
@@ -0,0 +1,15 @@
+---
+date: '2025-02-24'
+title: 'Search Engine'
+github: 'https://github.com/Roshan3580/Search-Engine'
+external: ''
+tech:
+  - Python
+  - NLTK
+  - BeautifulSoup
+  - PageRank
+  - TF-IDF
+showInProjects: true
+---
+
+Engineered a high-performance search engine prototype processing over 10,000 web documents, combining inverted indexing and graph-based ranking for efficient retrieval. Developed web crawling and indexing pipelines using Python, NLTK, and BeautifulSoup, achieving O(1) term lookups and eliminating 98% of duplicate pages via SHA-256 hashing. Integrated TF-IDF and PageRank algorithms to rank search results, reducing query response time to under 100 ms. Designed a scalable, modular system supporting future extensions like phrase queries and synonym-based expansion.
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
@@ -68,24 +67,12 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
     fetch('https://api.github.com/repos/bchiang7/v4')
       .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
       .catch(e => console.error(e));
   }, []);
 
@@ -107,27 +94,10 @@ const Footer = () => {
       <StyledCredit tabindex="-1">
         <a href="https://github.com/Roshan3580/portfolio">
           <div>- by Roshan Raj</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
         </a>
       </StyledCredit>
     </StyledFooter>
   );
-};
-
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
 };
 
 export default Footer;
