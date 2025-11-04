138a3a54a8a69f4a1bbb0b365b50af7c2b63286c
Roshan Raj <roshar1@uci.edu>
2025-11-04 13:49:45 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5667e5a43eecd783bcbf.js"
+    "url": "webpack-runtime-7a8e32e08405fede4d2e.js"
   },
   {
     "url": "framework-71f6327df07cb994cfb4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "407ebb42a0fefbd85d106f846357bca7"
+    "revision": "cd55039779e1a1b611c3c4220daa47f2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
